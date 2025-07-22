2cd9bd87de5e01ac2bfab822879af98a71720892
Ricardo Mesquita <ricardo.mesquita@actuar.com>
2025-07-22 15:22:52 -0300

    home

--- Frontend/src/app/pages/home/home.page.ts
+++ Frontend/src/app/pages/home/home.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  templateUrl: './home.page.html',
+  imports: [IonicModule, FormsModule, RouterModule], // IMPORTANTE
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
+  usuario = '';
+  senha = '';
+  erro = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    // Aqui você integraria com seu AuthService
+    if (this.usuario === 'admin' && this.senha === '1234') {
+      this.router.navigate(['/cadastro-propriedade']); // ou outra página
+    } else {
+      this.erro = 'Usuário ou senha inválidos';
+    }
+  }
+
+  irParaCadastro() {
+    this.router.navigate(['/cadastro-usuario']);
+  }
 }
